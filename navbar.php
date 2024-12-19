b0ac713849ee1c8cffc935675ff0b552a2aa3986
LAPTOP-BE7MBIHT\中島香織 <k.nakajima@mediavague.co.jp>
2024-12-19 17:05:03 +0900

    サロントップページ作成

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="SALON/index.php">SALON</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="#">お知らせ</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">予約</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">口コミ</a></li>
+        </ul>
+    </div>
+</nav>
