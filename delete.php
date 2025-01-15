2cf89cee5f80a5aa6ccc50d7ace7e3c9231d7ef3
LAPTOP-BE7MBIHT\中島香織 <k.nakajima@mediavague.co.jp>
2025-01-15 16:03:40 +0900

    データベース書き込み処理

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+$id = intval($_POST['id']);
+$pass = $_POST['pass'];
+
+if($id == '' || $pass==''){
+    header('Location:bbs.php');
+    exit();
+}
+
+$dsn='mysql:host=localhost;dbname=salon;charset=utf8';
+$user='user';
+$password='password';
+
+try{
+    $db = new PDO($dsn,$user,$password);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    $stmt = $db->prepare("DELETE FROM bbs WHERE id=:id AND pass=:pass");
+    $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+    $stmt->bindParam(':pass',$pass,PDO::PARAM_STR);
+
+    $stmt->execute();
+}catch(PDOException $e){
+    exit("エラー：".$e->getMessge());
+}
+header('Location:bbs.php');
+exit();
+?>
